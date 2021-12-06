293b55a603b832c0c54a4c31d3b625ea9027b199
zethan88 <zethan88@gmail.com>
2021-12-06 06:52:17 -0800

    all descriptions and temp visuals laid out, still working on visuals

---
+++ assets/js/timeline.js
@@ -0,0 +1,158 @@
+const margin = {top: 100, right: 30, bottom: 100, left: 30},
+    width = 1200 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
+
+const svg = d3.select("#timeline")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",`translate(${margin.left},${margin.top})`);
+
+const parseTime = d3.timeParse("%b/%d/%y");
+
+var div = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+d3.csv("../data/sf_shock_data_full_record.csv").then(function (data) {
+
+    data.forEach(function (d) {
+        d.Date = parseTime(d.date)
+    });
+
+    let xScale = d3.scaleBand()
+        .range([0, width])
+        .domain(d3.map(data, function (d) {return d.date;}))
+        .padding(0.09999)
+
+    svg.append("g")
+        .attr("transform", `translate(0, ${height/2})`)
+        .call(d3.axisBottom(xScale)
+            .tickValues([])
+        )
+        .selectAll("text")
+            //.attr("transform", "translate(0," + 100 + ")")
+            .style("text-anchor", "end")
+            .attr("dx", "-0.8em")
+            .attr("dy", "-0.5em")
+            .attr("transform", "rotate(-90)")
+
+    console.log(d3.map(data, function (d) {return d.date}));
+
+    let yScale = d3.scaleLinear()
+        .range([height, 0])
+        .domain(d3.extent(data, function (d) {return d.team1Score;}));
+
+    svg.append("g")
+        .attr("transform", `translate(0, ${-height/2})`)
+        .call(d3.axisLeft(yScale)
+            //.tickValues([])
+            .tickValues(['0', '1', '2', '3', '4'])
+            .tickFormat((d,i) => ['0', '1', '2', '3', '4'][i])
+        );
+
+    svg.append("g")
+        .attr("transform", `translate(0, ${height/2})`)
+        .call(d3.axisLeft(yScale)
+            //.tickValues([])
+            .tickValues(['4', '3', '2', '1', '0'])
+            .tickFormat((d,i) => ['0', '1', '2', '3', '4'][i])
+        );
+
+    //enemy scores
+    svg.append("g")
+        .selectAll(".bar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", function (d) {return xScale(d.date);})
+        .attr("y", function (d) {return -yScale(d.team2Score);})
+        .attr("width", xScale.bandwidth())
+        .attr("height", function (d) {
+            if(d.team2Score === "0" && d.team1Score === "3") {
+                return 0;
+            }
+            return height;})
+        .attr("fill", "#75787B")
+        .attr("transform", `translate(0, ${height/2})`)
+        .on("mouseover", function(event, d) {
+            d3.select(this)
+                .style("fill", d3.rgb("#75787B").darker(1))
+            div.transition()
+                .duration(200)
+                .style("opacity", .9);
+            div.html(d.date +
+                "<br/>" + d.team2 + " v. " + d.team1 +
+                "<br/>" + d.team2Score + " - " + d.team1Score +
+                "<br/>" + d.outcome)
+                .style("left", (event.pageX - 50) + "px")
+                .style("top", (event.pageY - 125) + "px")
+        })
+        .on("mouseout", function (d) {
+            d3.select(this)
+                .style("fill", "#75787B")
+            div.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
+
+    //sf scores
+    svg.append("g")
+        .selectAll(".bar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", function (d) {return xScale(d.date);})
+        .attr("y", function (d) {return yScale(d.team1Score);})
+        .attr("width", xScale.bandwidth())
+        .attr("height", function (d) {return height - yScale(d.team1Score);})
+        .attr("fill", "#FC4C02")
+        .attr("transform", `translate(0, ${-height/2})`)
+        .on("mouseover", function(event, d) {
+            d3.select(this)
+                .style("fill", d3.rgb("#FC4C02").darker(1))
+            div.transition()
+                .duration(200)
+                .style("opacity", .9);
+            div.html(d.date +
+                "<br/>" + d.team1 + " v. " + d.team2 +
+                "<br/>" + d.team1Score + " - " + d.team2Score +
+                "<br/>" + d.outcome)
+                .style("left", (event.pageX - 50) + "px")
+                .style("top", (event.pageY - 125) + "px")
+        })
+        .on("mouseout", function (d) {
+            d3.select(this)
+                .style("fill", "#FC4C02")
+            div.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
+
+    // svg.selectAll("rect")
+    //     .transition()
+    //     .duration(800)
+    //     .attr("y", function(d) { return yScale(d.team1Score); })
+    //     .attr("height", function(d) { return height - yScale(d.team1Score); })
+    //     .delay(function(d,i){console.log(i) ; return(i*100)})
+
+    //titles
+    svg.append("g")
+        .append("text")
+        .style("text-anchor", "end")
+        .attr("y", -60)
+        .attr("x", 150)
+        .attr("fill", "#FC4C02")
+        .attr("font-size", "18px")
+        .text("San Francisco Shock");
+
+    svg.append("g")
+        .append("text")
+        .style("text-anchor", "end")
+        .attr("y", 170)
+        .attr("x", 105)
+        .attr("fill", "#75787B")
+        .attr("font-size", "18px")
+        .text("Enemy Teams");
+})
--- assets/js/usage.js
+++ assets/js/usage.js
@@ -1,53 +1,95 @@
 
-var width = 400;
-var height = 200;
-var margin = {top: 45, right: 100, bottom: 20, left: 20};
+const margin = {top: 30, right: 0, bottom: 30, left: 50},
+    width = 600 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
 
-var xScale = d3.scaleBand()
-    .range([0, width])
-    .padding(0.1);
+d3.csv("../data/sf_shock_data_time_played(percent).csv").then(function (data) {
+    const groupdata = d3.group(data, d => d.player)
 
-var yScale = d3.scaleLinear()
-    .range([height, 0]);
+    const allKeys = new Set(data.map(function (d) {return d.player;}))
 
-var xAxis = d3.axisBottom()
-    .scale(xScale);
-
-d3.csv("../data/sf_shock_data_time_played(percent).csv").then (function (data) {
-    var uniquePlayers = Array.from(new Set(data.map(function (d) {
-        return d.player;
-    })));
-    xScale.domain(uniquePlayers);
-
-    var metrics = Array.from(
-        d3.group(data, d => d.hero), ([key, value]) => ({key, value})
-    );
-
-    var svg = d3.select("#usage").selectAll("svg")
-        .data(metrics)
+    const svg = d3.select("#usage")
+        .selectAll("svg")
+        .data(groupdata)
         .enter()
         .append("svg")
-        .attr("class", function (d) {return d.value[0].percent;});
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-    metrics.forEach(function (d, i) {
-        yScale.domain([0, d3.max(d.value, function (c) {return c.percent;})]);
+    const xScale = d3.scaleBand()
+        .domain(data.map(function (d) {return d.hero}))
+        .range([0, width])
+        .padding([0.5]);
 
-        d3.select("svg." + d.value[0].hero)
-            .selectAll('.bar')
-            .data(d.value)
-            .enter()
-            .append('rect')
-            .attr('class', 'bar')
-            .attr('x', function(c) { return xScale(c.player); })
-            .attr('width', xScale.bandwidth())
-            .attr('y', function(c) { return yScale(c.percent); })
-            .attr('height', function(c) { return height - yScale(c.percent); })
-            .attr('fill', 'teal');
+    svg.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale).ticks(3));
 
-        d3.select("svg." + d.value[0].hero)
-            .append('g')
-            .attr('transform', 'translate(0,' + height + ')')
-            .call(xAxis)
-    })
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, function (d) {return +d.percent;})])
+        .range([height, 0]);
+
+    svg.append("g")
+        .call(d3.axisLeft(yScale).ticks(5));
+
+    const color = d3.scaleSequential()
+        .interpolator(d3.interpolateBlues);
+
+    svg.append("g")
+        .selectAll(".bar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d) {return xScale(d.hero);})
+        .attr("y", function (d) {return yScale(d.percent);})
+        .attr("width", xScale.bandwidth())
+        .attr("height", function (d) {return height - yScale(d.percent);})
+        .attr("fill", function (d) {return color(d.percent);});
+
+    svg.append("text")
+        .attr("text-anchor", "start")
+        .attr("y", -5)
+        .attr("x", 0)
+        .text(function (d) {return(d[0])})
+
+
+    // var heroes = Array.from(new Set(data.map(function (d) {
+    //     return d.hero;
+    // })));
+    // xScale.domain(heroes);
+    //
+    // var metrics = Array.from(
+    //     d3.group(data, d => d.player), ([key, value]) => ({key, value})
+    // );
+    //
+    // var svg = d3.select("#usage").selectAll("svg")
+    //     .data(metrics)
+    //     .enter()
+    //     .append("svg")
+    //     .attr("class", function (d) {return d.value[0].percent;});
+    //
+    // metrics.forEach(function (d) {
+    //     yScale.domain([0, d3.max(d.value, function (c) {return c.percent;})]);
+    //
+    //     d3.select("svg." + d.value[0].hero)
+    //         .selectAll('.bar')
+    //         .data(d.value)
+    //         .enter()
+    //         .append('rect')
+    //         .attr('class', 'bar')
+    //         .attr('x', function(c) { return xScale(c.hero); })
+    //         .attr('width', xScale.bandwidth())
+    //         .attr('y', function(c) { return yScale(c.percent); })
+    //         .attr('height', function(c) { return height - yScale(c.percent); })
+    //         .attr('fill', 'teal');
+    //
+    //     d3.select("svg." + d.value[0].hero)
+    //         .append('g')
+    //         .attr('transform', 'translate(0,' + height + ')')
+    //         .call(xAxis)
+    // })
 })
 
